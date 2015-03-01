e3c960af40b3ff3a19c03067d8a30a46861e1ea3
Douglas Bentley <dee.bent.lee@gmail.com>
2015-03-01 22:28:40 +0200

    Direct threading experiment

---
+++ Experimentation/Threading/direct.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+//Direct Threading
+//================
+//Okay instead of part of our instruction being an opcode,
+//we just have a pointer to where the instruction happens.
+//I think args are stored in the next word
+
+int registers[6];
+
+int main()
+{
+	static void *program[] = {&&add, &&sub, &&add, &&halt}; //list of addresses in memory
+	
+	//get the address of the label (using &&) which is of type void (why??)
+	//(because...) void is the general pointer - used to store the address of any type of variable
+	//so it's like saying this is a pointer to something
+	
+	void *ptr;
+	
+	//make the pointer point to the start of the array
+	int pc = 0;
+	ptr = program[pc];
+	goto *ptr;
+	
+add:
+	printf("add\n");	
+	ptr = program[++pc];
+	goto *ptr;
+sub:
+	printf("sub\n");
+	ptr = program[++pc];
+	goto *ptr;
+halt:	
+	return 0;
+}
